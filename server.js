e3cb63e9d12fde7f956fd49c9d39f730d745fbba
Eric Shaw <ericshawmail@gmail.com>
2021-06-01 18:57:07 -0400

    file updated

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
